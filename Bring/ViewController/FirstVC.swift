4189da3d2d16b2ee1acb4820f16076ec73daa27e
김하늘 <skql8290@gmail.com>
2018-12-26 22:36:38 +0900

    로그인, 회원가입, 로고 뷰

---
+++ Bring/ViewController/FirstVC.swift
@@ -0,0 +1,35 @@
+//
+//  FirstVC.swift
+//  Bring
+//
+//  Created by Kim  Haneul on 2018. 12. 26..
+//  Copyright © 2018년 sohyeon. All rights reserved.
+//
+
+import UIKit
+
+class FirstVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func LoginAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func SignupAction(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
