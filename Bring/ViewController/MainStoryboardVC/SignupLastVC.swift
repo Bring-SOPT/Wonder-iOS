234a998bfe110d4eef15458c16728ec537bc5ff8
김하늘 <skql8290@gmail.com>
2018-12-29 17:07:21 +0900

    main.storyboard - serviceconfirm checkbox

--- Bring/ViewController/MainStoryboardVC/SignupLastVC.swift
+++ Bring/ViewController/MainStoryboardVC/SignupLastVC.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class SignupLastVC: UIViewController {
 
+    @IBOutlet weak var allServiceCheckbtn: UIButton!
+    @IBOutlet weak var serviceCheckbtn: UIButton!
+    @IBOutlet weak var privateCheckbtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
     }
+    
+    @IBAction func allServiceAction(_ sender: UIButton) {
+        
+        sender.setImage(UIImage(named:"allSelectedCheckbox.png"
+        ), for: UIControl.State.normal)
+        serviceCheckbtn.setImage(UIImage(named:"allSelectedCheckbox.png"
+        ), for: UIControl.State.normal)
+        privateCheckbtn.setImage(UIImage(named:"allSelectedCheckbox.png"
+        ), for: UIControl.State.normal)
+        
+        
+        
+    }
+    @IBAction func serviceAction(_ sender: UIButton) {
+    }
+    @IBAction func privateAction(_ sender: UIButton) {
+    }
+    
     
 
     @IBAction func signupFinishAction(_ sender: Any) {
