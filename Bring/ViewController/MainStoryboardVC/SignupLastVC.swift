6ee0f01b250626a51a5f476e909c68227bc6290c
김하늘 <skql8290@gmail.com>
2019-01-01 22:57:01 +0900

    assets ,   homeVC button code edit

--- Bring/ViewController/MainStoryboardVC/SignupLastVC.swift
+++ Bring/ViewController/MainStoryboardVC/SignupLastVC.swift
@@ -15,6 +15,10 @@ class SignupLastVC: UIViewController {
     @IBOutlet weak var privateCheckbtn: UIButton!
     
     
+    var allCheck:Bool = false
+    var serCheck:Bool = false
+    var priCheck:Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,19 +26,89 @@ class SignupLastVC: UIViewController {
     
     @IBAction func allServiceAction(_ sender: UIButton) {
         
-        sender.setImage(UIImage(named:"allSelectedCheckbox.png"
-        ), for: UIControl.State.normal)
-        serviceCheckbtn.setImage(UIImage(named:"allSelectedCheckbox.png"
-        ), for: UIControl.State.normal)
-        privateCheckbtn.setImage(UIImage(named:"allSelectedCheckbox.png"
-        ), for: UIControl.State.normal)
+        allCheck = !allCheck
         
+        if(allCheck){
+            
+            sender.setImage(UIImage(named:"allSelectedCheckbox.png"
+            ), for: UIControl.State.normal)
+            serviceCheckbtn.setImage(UIImage(named:"allSelectedCheckbox.png"
+            ), for: UIControl.State.normal)
+            privateCheckbtn.setImage(UIImage(named:"allSelectedCheckbox.png"
+            ), for: UIControl.State.normal)
+            
+            serCheck = true
+            priCheck = true
+        }
+            
+        else{
+            sender.setImage(UIImage(named:"checkBox.png"
+            ), for: UIControl.State.normal)
+            serviceCheckbtn.setImage(UIImage(named:"checkBox.png"
+            ), for: UIControl.State.normal)
+            privateCheckbtn.setImage(UIImage(named:"checkBox.png"
+            ), for: UIControl.State.normal)
+            
+            serCheck = false
+            priCheck = false
+        }
         
-        
+
     }
+    
     @IBAction func serviceAction(_ sender: UIButton) {
+        serCheck = !serCheck
+        
+        if(!serCheck){
+            if(allCheck){
+                
+                allCheck = false
+                serCheck = false
+                allServiceCheckbtn.setImage(UIImage(named:"checkBox.png"), for: UIControl.State.normal)
+                sender.setImage(UIImage(named:"checkBox.png"), for: UIControl.State.normal)
+            }
+            else{
+                serCheck = false
+                sender.setImage(UIImage(named:"checkBox.png"), for: UIControl.State.normal)
+            }
+        }
+            
+        else {//serCheck가 false일 떄
+            
+            serCheck = true
+            sender.setImage(UIImage(named:"allSelectedCheckbox.png"), for: UIControl.State.normal)
+            if(priCheck){
+                allCheck = true
+                allServiceCheckbtn.setImage(UIImage(named:"allSelectedCheckbox.png"), for: UIControl.State.normal)
+            }
+        }
     }
+    
     @IBAction func privateAction(_ sender: UIButton) {
+        priCheck = !priCheck
+        
+        if(!priCheck){
+            if(allCheck){
+                allCheck = false
+                priCheck = false
+                allServiceCheckbtn.setImage(UIImage(named:"checkBox.png"), for: UIControl.State.normal)
+                sender.setImage(UIImage(named:"checkBox.png"), for: UIControl.State.normal)
+            }
+            else{
+                priCheck = false
+                sender.setImage(UIImage(named:"checkBox.png"), for: UIControl.State.normal)
+            }
+        }
+            
+        else{
+            priCheck = true
+            sender.setImage(UIImage(named:"allSelectedCheckbox.png"), for: UIControl.State.normal)
+            if(serCheck){
+                allCheck = true
+                allServiceCheckbtn.setImage(UIImage(named:"allSelectedCheckbox.png"), for: UIControl.State.normal)
+            }
+        }
+
     }
     
     
