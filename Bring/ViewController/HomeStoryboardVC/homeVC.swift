441eadbd167d84ff41e0893dbfc78ff383e389b6
sohyeon <sohyun1018@gmail.com>
2019-01-02 19:13:21 +0900

    로그인 성공

--- Bring/ViewController/HomeStoryboardVC/homeVC.swift
+++ Bring/ViewController/HomeStoryboardVC/homeVC.swift
@@ -50,7 +50,7 @@ class homeVC: UIViewController, GMSMapViewDelegate, CLLocationManagerDelegate {
         searchController = UISearchController(searchResultsController: resultsViewController)
         searchController?.searchResultsUpdater = resultsViewController
         
-        let subView = UIView(frame: CGRect(x: 0, y: 0, width: 350.0, height: 45.0))
+        let subView = UIView(frame: CGRect(x: 0, y: 0, width: 350, height: 45.0))
         
         subView.addSubview((searchController?.searchBar)!)
         view.addSubview(subView)
